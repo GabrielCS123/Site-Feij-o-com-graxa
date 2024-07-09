d4cff810f192a2a91afda9a27925e1f652345ffd
GabrielCS123 <bielsouzacordeiro28@gmail.com>
2024-07-09 18:39:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function transformarLink(linkA) {
+    const url = new URL(linkA);
+    const videoId = url.pathname.split('/')[1];
+    const searchParams = url.search;
+    const linkB = `https://www.youtube.com/embed/${videoId}${searchParams}`;
+    return linkB;
+}
+
+let lsitaVideos = ['https://youtu.be/KDDOy5SyMQw?si=oCg9EtIev3WVcfYV',
+     'https://youtu.be/eDdlh8vwlas?si=TjrZkyLJheRQrHUM',
+      'https://youtu.be/Cq6u2y7B65U?si=fl6oKt8r9P3G_xUj',
+      'https://youtu.be/vrKecPTBMqE?si=XhvClgk8E9qIPcaj',
+      'https://youtu.be/yx_yQuQjOIo?si=pR7nR2J63A1fuQwQ'
+    ]
+
+
+function AdicionarVideos() {
+    const videoList = document.getElementById('videoList')
+    videoList.innerHTML = ''
+    lsitaVideos.forEach(link => {
+        const videoDiv = document.createElement('div')
+        videoDiv.className = 'video'
+        videoDiv.innerHTML = `
+            <iframe width="110%" height="120%" src="${transformarLink(link)}"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        `
+        videoList.appendChild(videoDiv);
+    })
+}
+
+AdicionarVideos()
